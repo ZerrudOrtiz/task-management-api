ce280b46e653dd8fbdba581591e99c9b5884205e
Raymart Ortiz <rzortiz@morepower.ph>
2025-02-21 00:35:38 +0800

    Added API for Task management

---
+++ database/migrations/2025_02_20_143258_create_tasks_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id('task_id');
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->date('due_date')->nullable();
+            $table->enum('status', ['pending', 'in_progress', 'completed','cancelled'])->default('pending');
+            $table->unsignedBigInteger('assigned_to');
+            $table->unsignedBigInteger('created_by');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
